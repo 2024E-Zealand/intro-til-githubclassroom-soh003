0b4c21bb61ada4492f2824e849e70c5896ede3f1
Søren Vinther Hansen <soh003@edu.zealand.dk>
2024-08-27 13:00:32 +0200

    instance properties konstruktør

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+        //instance
+        private string _name;
+        private int _age;
+
+        //Properties
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        //konstruktør
+        public Demo()
+        {
+            
+        }
+
+        public Demo(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
